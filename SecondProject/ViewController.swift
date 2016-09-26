df568ecb5d6a7b0818a99c8ea542b21b737854d9
spal7580 <psaineeraj@gmail.com>
2016-09-26 13:31:21 -0600

    random color

--- SecondProject/ViewController.swift
+++ SecondProject/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var texbutton
+        UIButton!
+    @IBOutlet weak var stepper: UIStepper!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    private func makeRandomcColor() -> UIColor
+    {
+        let randomcolor :  UIColor
+        
+        // color in swift is a percentage - 0-1 inclusive.
+        // of type CGFloat for all componenets: RGBA
+        let redColor : CGFloat = CGFloat(drand48())
+        let greenColor : CGFloat = CGFloat(double(arc4random_uniform(250))/255)
+        let blueColor : CGFloat = CGFloat(drand48())
+        let alphaChannel :  CGFloat = 1.0
+        
+        RandomColor = UIColor(red: redColor, green:  greenColor, blue: blueColor, alpha: alphaChannel)
+        
+        return randomcolor
+    }
 }
 
+ 
